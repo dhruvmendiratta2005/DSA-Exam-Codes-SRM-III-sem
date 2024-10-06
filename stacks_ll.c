2621bd186b47c24bfcb717ea995975ad4d90e8e9
dhruvmendiratta2005 <dhruvmendiratta2005@gmail.com>
2024-10-06 10:34:20 +0530

    stacks using ll done

---
+++ stacks_ll.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node * next;
+};
+struct Node * top=NULL;
+
+void push(int data){
+    struct Node * newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data=data;
+    if(top==NULL){
+        newNode->next=NULL;
+        top=newNode;
+    }
+    else{
+        newNode->next=top;
+        top=newNode;
+    }
+}
+
+void pop(){
+    if(top==NULL){
+        printf("\n Stack is EMPTY \n");
+    }
+    int element=top->data;
+    struct Node * temp=top;
+    top=top->next;
+    free(temp);
+    printf("\nThe popped element is : %d",element);
+}
+
+void display(){
+    if(top==NULL){
+        printf("\nStack Underflow");
+    }else{
+        struct Node * temp=top;
+        while(temp!=NULL){
+            printf("\n%d",temp->data);
+            temp=temp->next;
+        }
+    }
+}
+void peek(){
+    if(top==NULL){
+        printf("\nStack is empty");
+    }
+    else{
+        int element=top->data;
+        printf("\nTop Element: %d", element);
+    }
+}
+int main(){
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    peek();
+    pop();
+    display();
+}
